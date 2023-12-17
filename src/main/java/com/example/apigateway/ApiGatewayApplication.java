480aad2af01bdd42389ed9db17b2b406fbe424dd
Matheus Pereira <matheuspereira.azapfy@gmail.com>
2023-12-17 18:38:03 -0300

    primeiro commit

---
+++ src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,30 @@
+package com.example.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+						.path("/motoristas/**")
+						.uri("lb://motorista-service"))
+				.route(p -> p
+						.path("/fretes/**")
+						.uri("lb://frete-service"))
+				.build();
+	}
+
+}
